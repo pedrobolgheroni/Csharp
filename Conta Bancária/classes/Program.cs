e3a80548c94221a03f11cc600ea2d36dd775ab6a
Pedro Bolgheroni <pedrobolgheroni@gmail.com>
2023-03-28 20:01:35 -0300

    Add files via upload

---
+++ Conta Bancária/classes/Program.cs
@@ -0,0 +1,11 @@
+﻿using classes;
+
+var conta = new ContaBancaria("Pedro", 1000.50m); // "m" de monetário, vai ser tratado como moeda
+
+Console.Write($"A conta {conta.Numero} foi criada por {conta.Cliente} com saldo inicial de {conta.Saldo}\n\n");
+
+conta.EfetuarSaque(500, DateTime.Now, "Pagamento de aluguel");
+Console.WriteLine($"Saldo Atual de {conta.Saldo}\n");
+
+conta.EfetuarDeposito(100, DateTime.Now, "Recebimento de um amigo");
+Console.WriteLine($"Saldo Atual de {conta.Saldo}\n");
