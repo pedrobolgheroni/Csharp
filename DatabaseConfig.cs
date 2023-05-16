cede40fd7d8f02a062934f468e0f3e9203c2c80c
Pedro Bolgheroni <pedrobolgheroni@gmail.com>
2023-05-16 14:58:13 -0300

    Add files via upload
    Acesso ao BDD

---
+++ DatabaseConfig.cs
@@ -0,0 +1,5 @@
+namespace Aula09DB.Database;
+class DatabaseConfig
+{
+    public string ConnectionString { get => "Data Source=database.db"; } // colocando na variável ConnectionString a conexão ao bdd
+}
