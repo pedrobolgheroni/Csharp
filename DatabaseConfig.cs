deee0d6caf80a4cfd129ff035bd10245ed54a844
Pedro Bolgheroni <pedrobolgheroni@gmail.com>
2023-05-16 14:59:14 -0300

    Delete DatabaseConfig.cs

--- DatabaseConfig.cs
+++
@@ -1,5 +0,0 @@
-namespace Aula09DB.Database;
-class DatabaseConfig
-{
-    public string ConnectionString { get => "Data Source=database.db"; } // colocando na variável ConnectionString a conexão ao bdd
-}
