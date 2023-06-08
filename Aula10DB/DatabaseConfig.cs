fc71f2df5262d32b5a366f7edc84227bad6f3158
Pedro Bolgheroni <pedrobolgheroni@gmail.com>
2023-06-08 11:21:05 -0300

    Update DatabaseConfig.cs

--- Aula10DB/DatabaseConfig.cs
+++ Aula10DB/DatabaseConfig.cs
@@ -1,5 +1,5 @@
 namespace Aula10DB.Database;
 class DatabaseConfig
 {
-    public string ConnectionString {get => "Server=localhost;Port=3306;Database=aula10DB;Uid=root;Pwd=pedro2006;"; } // colocando na variável ConnectionString a conexão ao bdd
+    public string ConnectionString {get => "Server=seu_servidor;Port=sua_porta;Database=seu_banco_de_dados;Uid=seu_usuario;Pwd=sua_senha;"; } // colocando na variável ConnectionString a conexão ao bdd
 }
